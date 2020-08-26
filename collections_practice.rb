b7ad75fa88d95d0822d23acef8cb4e0e577dee50
Ryan Altimari <ryanaltimari8@gmail.com>
2020-08-26 11:38:37 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,44 @@
 
+def sort_array_asc(arr)
+  arr.sort 
+end
+
+def sort_array_desc(arr)
+  arr.sort! {|x, y| y <=> x}
+end
+
+def sort_array_char_count(arr)
+  result = arr.sort {|left, right| left.length <=> right.length}
+  result
+end
+
+def swap_elements(arr)
+  arr[1], arr[2] = arr[2], arr[1]
+  arr
+end
+
+def reverse_array(arr)
+  new = arr.reverse 
+  new
+end
+
+def kesha_maker(arr)
+  answer = []
+  arr.each do |i|
+   answer << i[2] = "$"
+  end
+end
+
+def find_a(arr)
+  with_a = []
+  i = 0 
+  while i < arr.length
+     with_a << arr[i].start_with?("a")
+     i += 1
+  end
+  with_a
+end
+
+
+
+
